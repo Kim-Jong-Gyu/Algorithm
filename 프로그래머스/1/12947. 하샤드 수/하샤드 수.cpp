63856dfc0805533ad83bdeaa71b6d932e92c951e
jonggyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2023-11-16 01:41:58 +0900

    Update 하샤드 수.cpp

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -2,16 +2,12 @@
 #include <vector>
 
 using namespace std;
-vector<int> v;
 int sum, tmp;
 bool solution(int x) {
     tmp = x;
     while(tmp > 0){
-        v.push_back(tmp % 10);
+        sum +=tmp % 10;
         tmp /= 10;
-    }
-    for(int a : v){
-       sum+= a; 
     }
     if(x % sum == 0)
         return true;
