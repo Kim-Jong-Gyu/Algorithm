89317e1717486f1d743a2bc9a17f6b7c9d1dfe03
jonggyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2023-12-15 23:49:28 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,12 +1,16 @@
 class Solution {
     public long solution(int n) {
-        long[] dp = new long[n + 2];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i <= n; i++){
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        long answer = 0;
+
+        if (n <= 2) 
+            return n;
+        long prevPrev = 1;
+        long prev = 2;
+        for (int i = 3; i <= n; i++) {
+            answer = (prev + prevPrev) % 1234567;
+            prevPrev = prev;
+            prev = answer;
         }
-        return dp[n];
+        return answer;
     }
 }
