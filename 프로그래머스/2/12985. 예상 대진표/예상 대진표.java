1abe5f97839acb8c34d724626fb013b8432bd0ce
jonggyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2024-08-19 10:15:00 +0900

    [level 2] Title: 예상 대진표, Time: 0.40 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while(true){
+            a = (int)Math.ceil((double) a / 2);
+            b = (int)Math.ceil((double) b / 2);
+            answer++;
+            if(a == b){
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
