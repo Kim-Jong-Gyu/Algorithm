9826447d0cf7126324cabf11662215a1955b6e9a
jonggyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2024-12-04 18:42:11 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+	static int n;
+	static int[][] input;
+
+	static int[][] mem;
+
+	static int[] dp;
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+
+		input = new int[n + 1][2];
+		mem = new int[n + 1][n + 1];
+		dp = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			// 걸린 시간
+			input[i][0] = Integer.parseInt(st.nextToken());
+			// 금액
+			input[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		// 요일
+		for (int i = 1; i <= n; i++) {
+			dp[i] = dp[i - 1];
+			for (int j = 1; j <= n; j++) {
+				if(input[j][0] + j == i + 1) {
+					mem[i][j] = input[j][1] + dp[j - 1];
+					dp[i] = Math.max(dp[i], mem[i][j]);
+				}
+				else {
+					mem[i][j] = dp[i];
+				}
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
